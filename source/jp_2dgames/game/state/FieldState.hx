d2af621f6bd3f2005d15f65a34960639d113b9c0
syun77 <kenmomomomomo@gmail.com>
2015-09-27 15:28:57 +0900

    fixed #128 TournRPG-128 フィールド画面の追加

---
+++ source/jp_2dgames/game/state/FieldState.hx
@@ -0,0 +1,28 @@
+package jp_2dgames.game.state;
+import flixel.FlxState;
+
+/**
+ * フィールドシーン
+ **/
+class FieldState extends FlxState {
+  /**
+   * 生成
+   **/
+  override public function create():Void {
+    super.create();
+  }
+
+  /**
+   * 破棄
+   */
+  override public function destroy():Void {
+    super.destroy();
+  }
+
+  /**
+   * 更新
+   */
+  override public function update():Void {
+    super.update();
+  }
+}
